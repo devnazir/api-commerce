00e3eb945b26f9b657662b54230762161440f4a1
devnazir <nazirbachrululum@gmail.com>
2021-05-01 12:00:30 +0700

    fix: cannot GET /

--- routes/products.js
+++ routes/products.js
@@ -9,4 +9,10 @@ router
   .patch('/products/:id', updateProduct)
   .delete('/products/:id', deleteProduct)
 
+router.use((req, res) => {
+  res.status(200).json({
+    status: true
+  })
+})
+
 module.exports = router
